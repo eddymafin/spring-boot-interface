9757a85127f748ba4be9b6353f5e2f4f3a2dd1ba
eddymafin <marumai2451@gmail.com>
2025-07-07 10:12:34 +0900

    create project

---
+++ src/main/java/com/example/demo/SpringStartInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStartInterfaceApplication.class, args);
+	}
+
+}
